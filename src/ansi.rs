earthlings-dev/alacritty

commit 2920cbe7103f03a45080bfb7610bd7f481f36361

Add clippy check to travis
This commit adds clippy as a required step of the build process. To make
this possible, all existing clippy issues have been resolved.

src/ansi.rs
@@ -360,6 +360,9 @@ pub enum CursorStyle {
 
     /// Cursor is a vertical bar `⎸`
     Beam,
+
+    /// Cursor is a box like `☐`
+    HollowBlock,
 }
 
 impl Default for CursorStyle {
@@ -731,18 +734,16 @@ impl<'a, H, W> vte::Perform for Performer<'a, H, W>
     // TODO replace OSC parsing with parser combinators
     #[inline]
     fn osc_dispatch(&mut self, params: &[&[u8]]) {
-        macro_rules! unhandled {
-            () => {{
-                let mut buf = String::new();
-                for items in params {
-                    buf.push_str("[");
-                    for item in *items {
-                        buf.push_str(&format!("{:?},", *item as char));
-                    }
-                    buf.push_str("],");
+        fn unhandled(params: &[&[u8]]) {
+            let mut buf = String::new();
+            for items in params {
+                buf.push_str("[");
+                for item in *items {
+                    buf.push_str(&format!("{:?},", *item as char));
                 }
-                warn!("[unhandled osc_dispatch]: [{}] at line {}", &buf, line!());
-            }}
+                buf.push_str("],");
+            }
+            warn!("[unhandled osc_dispatch]: [{}] at line {}", &buf, line!());
         }
 
         if params.is_empty() || params[0].is_empty() {
@@ -752,13 +753,13 @@ impl<'a, H, W> vte::Perform for Performer<'a, H, W>
         match params[0] {
             // Set window title
             b"0" | b"2" => {
-                if params.len() < 2 {
-                    return unhandled!();
+                if params.len() >= 2 {
+                    if let Ok(utf8_title) = str::from_utf8(params[1]) {
+                        self.handler.set_title(utf8_title);
+                        return;
+                    }
                 }
-
-                if let Ok(utf8_title) = str::from_utf8(params[1]) {
-                    self.handler.set_title(utf8_title);
-                }
+                unhandled(params);
             },
 
             // Set icon name
@@ -767,70 +768,60 @@ impl<'a, H, W> vte::Perform for Performer<'a, H, W>
 
             // Set color index
             b"4" => {
-                if params.len() == 1 || params.len() % 2 == 0 {
-                    return unhandled!();
-                }
-
-                for chunk in params[1..].chunks(2) {
-                    if let Some(index) = parse_number(chunk[0]) {
-                        if let Some(color) = parse_rgb_color(chunk[1]) {
-                            self.handler.set_color(index as usize, color);
-                        } else {
-                            unhandled!();
+                if params.len() > 1 && params.len() % 2 != 0 {
+                    for chunk in params[1..].chunks(2) {
+                        let index = parse_number(chunk[0]);
+                        let color = parse_rgb_color(chunk[0]);
+                        if let (Some(i), Some(c)) = (index, color) {
+                            self.handler.set_color(i as usize, c);
+                            return;
                         }
-                    } else {
-                        unhandled!();
                     }
                 }
+                unhandled(params);
             }
 
             // Set foreground color
             b"10" => {
-                if params.len() < 2 {
-                    return unhandled!();
+                if params.len() >= 2 {
+                    if let Some(color) = parse_rgb_color(params[1]) {
+                        self.handler.set_color(NamedColor::Foreground as usize, color);
+                        return;
+                    }
                 }
-
-                if let Some(color) = parse_rgb_color(params[1]) {
-                    self.handler.set_color(NamedColor::Foreground as usize, color);
-                } else {
-                    unhandled!()
-                }
+                unhandled(params);
             }
 
             // Set background color
             b"11" => {
-                if params.len() < 2 {
-                    return unhandled!();
+                if params.len() >= 2 {
+                    if let Some(color) = parse_rgb_color(params[1]) {
+                        self.handler.set_color(NamedColor::Background as usize, color);
+                        return;
+                    }
                 }
-
-                if let Some(color) = parse_rgb_color(params[1]) {
-                    self.handler.set_color(NamedColor::Background as usize, color);
-                } else {
-                    unhandled!()
-                }
+                unhandled(params);
             }
 
             // Set text cursor color
             b"12" => {
-                if params.len() < 2 {
-                    return unhandled!();
+                if params.len() >= 2 {
+                    if let Some(color) = parse_rgb_color(params[1]) {
+                        self.handler.set_color(NamedColor::Cursor as usize, color);
+                        return;
+                    }
                 }
-
-                if let Some(color) = parse_rgb_color(params[1]) {
-                    self.handler.set_color(NamedColor::Cursor as usize, color);
-                } else {
-                    unhandled!()
-                }
+                unhandled(params);
             }
 
             // Set clipboard
             b"52" => {
                 if params.len() < 3 {
-                    return unhandled!();
+                    return unhandled(params);
                 }
 
                 match params[2] {
-                    b"?" => unhandled!(),
+                    b"?" => unhandled(params),
                     selection => {
                         if let Ok(string) = base64::decode(selection) {
                             if let Ok(utf8_string) = str::from_utf8(&string) {
@@ -855,29 +846,21 @@ impl<'a, H, W> vte::Perform for Performer<'a, H, W>
                 for param in &params[1..] {
                     match parse_number(param) {
                         Some(index) => self.handler.reset_color(index as usize),
-                        None => unhandled!(),
+                        None => unhandled(params),
                     }
                 }
             }
 
             // Reset foreground color
-            b"110" => {
-                self.handler.reset_color(NamedColor::Foreground as usize);
-            }
+            b"110" => self.handler.reset_color(NamedColor::Foreground as usize),
 
             // Reset background color
-            b"111" => {
-                self.handler.reset_color(NamedColor::Background as usize);
-            }
+            b"111" => self.handler.reset_color(NamedColor::Background as usize),
 
             // Reset text cursor color
-            b"112" => {
-                self.handler.reset_color(NamedColor::Cursor as usize);
-            }
+            b"112" => self.handler.reset_color(NamedColor::Cursor as usize),
 
-            _ => {
-                unhandled!();
-            }
+            _ => unhandled(params),
         }
     }
 
@@ -1389,7 +1372,7 @@ mod tests {
     use super::{Processor, Handler, Attr, TermInfo, Color, StandardCharset, CharsetIndex, parse_rgb_color, parse_number};
     use ::Rgb;
 
-    /// The /dev/null of io::Write
+    /// The /dev/null of `io::Write`
     struct Void;
 
     impl io::Write for Void {

.travis.yml
@@ -11,10 +11,19 @@ rust:
   - stable
   - nightly
 
+env:
+  - FEATURES="clippy"
+  - FEATURES=""
+
 matrix:
   fast_finish: true
+  exclude:
+    - rust: stable
+      env: FEATURES="clippy"
+    - rust: nightly
+      env: FEATURES=""
   allow_failures:
     - rust: nightly
 
 script:
-  - cargo test --no-default-features
+  - cargo test --no-default-features --features "$FEATURES"

src/config.rs
@@ -178,7 +178,7 @@ impl VisualBellConfig {
     /// Visual bell duration in milliseconds
     #[inline]
     pub fn duration(&self) -> Duration {
-        Duration::from_millis(self.duration as u64)
+        Duration::from_millis(u64::from(self.duration))
     }
 }
 
@@ -240,7 +240,7 @@ impl Alpha {
         self.0 = Self::clamp_to_valid_range(value);
     }
 
-    #[inline(always)]
+    #[inline]
     pub fn get(&self) -> f32 {
         self.0
     }
@@ -1152,8 +1152,7 @@ impl FromStr for Rgb {
 impl ::std::error::Error for Error {
     fn cause(&self) -> Option<&::std::error::Error> {
         match *self {
-            Error::NotFound => None,
-            Error::Empty => None,
+            Error::NotFound | Error::Empty => None,
             Error::ReadingEnvHome(ref err) => Some(err),
             Error::Io(ref err) => Some(err),
             Error::Yaml(ref err) => Some(err),
@@ -1174,8 +1173,7 @@ impl ::std::error::Error for Error {
 impl ::std::fmt::Display for Error {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
         match *self {
-            Error::NotFound => write!(f, "{}", ::std::error::Error::description(self)),
-            Error::Empty => write!(f, "{}", ::std::error::Error::description(self)),
+            Error::NotFound | Error::Empty => write!(f, "{}", ::std::error::Error::description(self)),
             Error::ReadingEnvHome(ref err) => {
                 write!(f, "could not read $HOME environment variable: {}", err)
             },
@@ -1218,7 +1216,7 @@ impl Config {
     /// 2. $XDG_CONFIG_HOME/alacritty.yml
     /// 3. $HOME/.config/alacritty/alacritty.yml
     /// 4. $HOME/.alacritty.yml
-    pub fn installed_config() -> Option<Cow<'static, Path>> {
+    pub fn installed_config<'a>() -> Option<Cow<'a, Path>> {
         // Try using XDG location by default
         ::xdg::BaseDirectories::with_prefix("alacritty")
             .ok()

src/display.rs
@@ -228,7 +228,7 @@ impl Display {
             })?;
 
             let stop = init_start.elapsed();
-            let stop_f = stop.as_secs() as f64 + stop.subsec_nanos() as f64 / 1_000_000_000f64;
+            let stop_f = stop.as_secs() as f64 + f64::from(stop.subsec_nanos()) / 1_000_000_000f64;
             info!("Finished initializing glyph cache in {}", stop_f);
 
             cache
@@ -238,8 +238,8 @@ impl Display {
         // font metrics should be computed before creating the window in the first
         // place so that a resize is not needed.
         let metrics = glyph_cache.font_metrics();
-        let cell_width = (metrics.average_advance + font.offset().x as f64) as u32;
-        let cell_height = (metrics.line_height + font.offset().y as f64) as u32;
+        let cell_width = (metrics.average_advance + f64::from(font.offset().x)) as u32;
+        let cell_height = (metrics.line_height + f64::from(font.offset().y)) as u32;
 
         Ok((glyph_cache, cell_width as f32, cell_height as f32))
     }
@@ -252,8 +252,8 @@ impl Display {
         });
 
         let metrics = cache.font_metrics();
-        self.size_info.cell_width = ((metrics.average_advance + config.font().offset().x as f64) as f32).floor();
-        self.size_info.cell_height = ((metrics.line_height + config.font().offset().y as f64) as f32).floor();
+        self.size_info.cell_width = ((metrics.average_advance + f64::from(config.font().offset().x)) as f32).floor();
+        self.size_info.cell_height = ((metrics.line_height + f64::from(config.font().offset().y)) as f32).floor();
     }
 
     #[inline]

src/event.rs
@@ -56,8 +56,8 @@ impl<'a, N: Notify + 'a> input::ActionContext for ActionContext<'a, N> {
     }
 
     fn copy_selection(&self, buffer: ::copypasta::Buffer) {
-        if let &mut Some(ref selection) = self.selection {
-            selection.to_span(self.terminal as &Term)
+        if let Some(ref selection) = *self.selection {
+            selection.to_span(self.terminal)
                 .map(|span| {
                     let buf = self.terminal.string_from_selection(&span);
                     if !buf.is_empty() {
@@ -79,7 +79,7 @@ impl<'a, N: Notify + 'a> input::ActionContext for ActionContext<'a, N> {
     fn update_selection(&mut self, point: Point, side: Side) {
         self.selection_modified = true;
         // Update selection if one exists
-        if let &mut Some(ref mut selection) = self.selection {
+        if let Some(ref mut selection) = *self.selection {
             selection.update(point, side);
             return;
         }
@@ -94,7 +94,7 @@ impl<'a, N: Notify + 'a> input::ActionContext for ActionContext<'a, N> {
     }
 
     fn semantic_selection(&mut self, point: Point) {
-        *self.selection = Some(Selection::semantic(point, self.terminal as &Term));
+        *self.selection = Some(Selection::semantic(point, self.terminal));
         self.selection_modified = true;
     }
 
@@ -255,8 +255,7 @@ impl<N: Notify> Processor<N> {
     ) {
         match event {
             // Pass on device events
-            Event::DeviceEvent { .. } => (),
-            Event::Suspended { .. } => (),
+            Event::DeviceEvent { .. } | Event::Suspended { .. } => (),
             Event::WindowEvent { event, .. } => {
                 use glutin::WindowEvent::*;
                 match event {

src/event_loop.rs
@@ -90,9 +90,8 @@ impl event::Notify for Notifier {
         if bytes.len() == 0 {
             return
         }
-        match self.0.send(Msg::Input(bytes)) {
-            Ok(_) => (),
-            Err(_) => panic!("expected send event loop msg"),
+        if self.0.send(Msg::Input(bytes)).is_err() {
+            panic!("expected send event loop msg");
         }
     }
 }

src/grid.rs
@@ -151,14 +151,14 @@ impl<T> Grid<T> {
     }
 
     #[inline]
-    pub fn scroll_down(&mut self, region: Range<index::Line>, positions: index::Line) {
+    pub fn scroll_down(&mut self, region: &Range<index::Line>, positions: index::Line) {
         for line in IndexRange((region.start + positions)..region.end).rev() {
             self.swap_lines(line, line - positions);
         }
     }
 
     #[inline]
-    pub fn scroll_up(&mut self, region: Range<index::Line>, positions: index::Line) {
+    pub fn scroll_up(&mut self, region: &Range<index::Line>, positions: index::Line) {
         for line in IndexRange(region.start..(region.end - positions)) {
             self.swap_lines(line, line + positions);
         }
@@ -618,7 +618,7 @@ mod tests {
 
         info!("grid: {:?}", grid);
 
-        grid.scroll_up(Line(0)..Line(10), Line(2));
+        grid.scroll_up(&(Line(0)..Line(10)), Line(2));
 
         info!("grid: {:?}", grid);
 
@@ -652,7 +652,7 @@ mod tests {
 
         info!("grid: {:?}", grid);
 
-        grid.scroll_down(Line(0)..Line(10), Line(2));
+        grid.scroll_down(&(Line(0)..Line(10)), Line(2));
 
         info!("grid: {:?}", grid);
 

src/index.rs
@@ -88,18 +88,18 @@ impl fmt::Display for Linear {
     }
 }
 
-/// Copyright 2015 The Rust Project Developers. See the COPYRIGHT
-/// file at the top-level directory of this distribution and at
-/// http://rust-lang.org/COPYRIGHT.
-///
-/// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-/// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-/// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-/// option. This file may not be copied, modified, or distributed
-/// except according to those terms.
-///
-/// implements binary operators "&T op U", "T op &U", "&T op &U"
-/// based on "T op U" where T and U are expected to be `Copy`able
+// Copyright 2015 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+//
+// implements binary operators "&T op U", "T op &U", "&T op &U"
+// based on "T op U" where T and U are expected to be `Copy`able
 macro_rules! forward_ref_binop {
     (impl $imp:ident, $method:ident for $t:ty, $u:ty) => {
         impl<'a> $imp<$u> for &'a $t {

commit 901859d39fde157584ab7758c28fa29e5254e13f

Fix indexed color setting regression
Commit 2920cbe7103f03a45080bfb7610bd7f481f36361 introduced a regression because of a typo in the chunk slice index for the `parse_rgb_color` call.

This fixes this issue by resetting it to the state it was before the faulty commit.

src/ansi.rs
@@ -771,7 +771,7 @@ impl<'a, H, W> vte::Perform for Performer<'a, H, W>
                 if params.len() > 1 && params.len() % 2 != 0 {
                     for chunk in params[1..].chunks(2) {
                         let index = parse_number(chunk[0]);
-                        let color = parse_rgb_color(chunk[0]);
+                        let color = parse_rgb_color(chunk[1]);
                         if let (Some(i), Some(c)) = (index, color) {
                             self.handler.set_color(i as usize, c);
                             return;