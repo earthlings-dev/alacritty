earthlings-dev/alacritty

commit e99057b179cbdc0851c36575dd043fcdaa45437a

Fix crash when resizing Alacritty
Fixes #3088.

alacritty_terminal/src/grid/storage.rs
@@ -254,7 +254,7 @@ impl<T> Storage<T> {
         debug_assert!(count.abs() as usize <= self.inner.len());
 
         let len = self.inner.len();
-        self.zero = self.wrap_index((self.zero as isize + count + len as isize) as usize);
+        self.zero = (self.zero as isize + count + len as isize) as usize % self.len;
     }
 
     /// Rotate the grid up, moving all existing lines down in history.
@@ -837,5 +837,23 @@ mod test {
         assert_eq!(storage.inner, shrinking_expected.inner);
         assert_eq!(storage.zero, shrinking_expected.zero);
         assert_eq!(storage.len, shrinking_expected.len);
+    }
+
+    #[test]
+    fn rotate_wrap_zero() {
+        let mut storage = Storage {
+            inner: vec![
+                Row::new(Column(1), &'-'),
+                Row::new(Column(1), &'-'),
+                Row::new(Column(1), &'-'),
+            ],
+            zero: 2,
+            visible_lines: Line(0),
+            len: 3,
+        };
+
+        storage.rotate(2);
+
+        assert!(storage.zero < storage.inner.len());
     }
 }

commit 8cf3f1934660d30d54760f23d7b320a7d23e1d12

Fix storage rotation error
This fixes a regression introduced in
e99057b179cbdc0851c36575dd043fcdaa45437a, which used `self.len` to
calculate the remainder of `self.zero` during rotation instead of
`self.inner.len()`, leading to a broken `self.zero` offset and incorrect
rotation.

alacritty_terminal/src/grid/storage.rs
@@ -205,7 +205,18 @@ impl<T> Storage<T> {
     #[inline]
     fn compute_index(&self, requested: usize) -> usize {
         debug_assert!(requested < self.len);
-        self.wrap_index(self.zero + requested)
+
+        let zeroed = self.zero + requested;
+
+        // Use if/else instead of remainder here to improve performance.
+        //
+        // Requires `zeroed` to be smaller than `self.inner.len() * 2`,
+        // but both `self.zero` and `requested` are always smaller than `self.inner.len()`.
+        if zeroed >= self.inner.len() {
+            zeroed - self.inner.len()
+        } else {
+            zeroed
+        }
     }
 
     pub fn swap_lines(&mut self, a: Line, b: Line) {
@@ -254,7 +265,7 @@ impl<T> Storage<T> {
         debug_assert!(count.abs() as usize <= self.inner.len());
 
         let len = self.inner.len();
-        self.zero = (self.zero as isize + count + len as isize) as usize % self.len;
+        self.zero = (self.zero as isize + count + len as isize) as usize % self.inner.len();
     }
 
     /// Rotate the grid up, moving all existing lines down in history.
@@ -262,7 +273,7 @@ impl<T> Storage<T> {
     /// This is a faster, specialized version of [`rotate`].
     #[inline]
     pub fn rotate_up(&mut self, count: usize) {
-        self.zero = self.wrap_index(self.zero + count);
+        self.zero = (self.zero + count) % self.inner.len();
     }
 
     /// Drain all rows in the grid.
@@ -276,19 +287,6 @@ impl<T> Storage<T> {
         self.len = vec.len();
         self.inner = vec;
         self.zero = 0;
-    }
-
-    /// Wrap index to be within the inner buffer.
-    ///
-    /// This uses if/else instead of the remainder to improve performance,
-    /// so the assumption is made that `index < self.inner.len() * 2`.
-    #[inline]
-    fn wrap_index(&self, index: usize) -> usize {
-        if index >= self.inner.len() {
-            index - self.inner.len()
-        } else {
-            index
-        }
     }
 }
 